04c1ee0fc16f1a18c4a07818ef2a5774fa1485ab
dimasm93 <software.dimas_m@icloud.com>
2021-06-22 12:45:13 +0700

    enable kafka config

--- src/main/java/com/maryanto/dimas/example/SpringbootKafkaExampleApplication.java
+++ src/main/java/com/maryanto/dimas/example/SpringbootKafkaExampleApplication.java
@@ -2,7 +2,9 @@ package com.maryanto.dimas.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class SpringbootKafkaExampleApplication {
 
